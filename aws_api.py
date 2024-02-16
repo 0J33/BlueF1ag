5f8737fc4fa118f0b06dec2c6aa0cbb69102dcaf
Ojee <omargamal.02.11@gmail.com>
2024-02-16 23:58:26 +0200

    fix car data and tel

--- aws_api.py
+++ aws_api.py
@@ -239,6 +239,77 @@ def save_results(yr, rc, sn):
         print(traceback.format_exc())
         print("error", str(exc)) 
         
+def save_car_data(yr, rc, sn):
+
+    folder_name = f"{yr}_{rc}_{sn}"
+        
+    try:
+        
+        if check_folder_exists(f"car_data/{folder_name}"):
+            raise Exception("Folder already exists")
+        
+        if "test" in rc.lower() or "pre-season" in rc.lower():
+            raise Exception("Test session")
+        
+        if rc == "Sprint Shootout":
+            raise Exception("Sprint Shootout")
+
+        session = get_sess(yr, rc, sn)
+        session.load()
+        laps = session.laps
+        
+        try:
+            os.mkdir("data_dump/" + str(yr) + "_" + str(rc) + "_" + str(sn))
+        except:
+            pass
+        
+         
+        drivers = laps['Driver'].unique()
+        
+        for driver in drivers:
+            lap = 1
+            while lap <= int(laps[laps['Driver'] == driver]['LapNumber'].max()) + 1:
+                try:
+                    driver_laps = session.laps.pick_driver(driver)
+                    fast = driver_laps[driver_laps['LapNumber'] == int(lap)].iloc[0]
+                    car_data = fast.get_car_data().add_distance()
+                    
+                    csv_file_name = f"{yr}_{rc}_{sn}_{driver}_{lap}_car_data.csv"
+                    car_data.to_csv("data_dump/" + str(yr) + "_" + str(rc) + "_" + str(sn) + "/" + csv_file_name, index=False)
+                    
+                except:
+                    pass
+                lap += 1
+        
+        summary_data = []
+        for driver in drivers:
+            # find max numver of laps for driver and loop through
+            for lap in range(1, int(laps[laps['Driver'] == driver]['LapNumber'].max()) + 1):
+                summary_data.append({'driver': driver, 'lap': lap})
+        summary_df = pd.DataFrame(summary_data)
+        summary_file_name = f"{yr}_{rc}_{sn}_summary.csv"
+        summary_df.to_csv("data_dump/" + str(yr) + "_" + str(rc) + "_" + str(sn) + "/" + summary_file_name, index=False)
+        
+        for driver in drivers:
+            max_lap = int(laps[laps['Driver'] == driver]['LapNumber'].max())
+            for lap in range(1, max_lap + 1):
+                try:
+                    csv_file_name = f"{yr}_{rc}_{sn}_{driver}_{lap}_car_data.csv"
+                    upload_file("data_dump/" + str(yr) + "_" + str(rc) + "_" + str(sn) + "/" + csv_file_name, csv_file_name, f"car_data/{folder_name}/")
+                    print("Uploaded", csv_file_name)
+                except:
+                    pass
+        
+        upload_file("data_dump/" + str(yr) + "_" + str(rc) + "_" + str(sn) + "/" + summary_file_name, summary_file_name, f"car_data/{folder_name}/")
+        for file in os.listdir("data_dump/" + str(yr) + "_" + str(rc) + "_" + str(sn)):
+            delete_file_local(str(yr) + "_" + str(rc) + "_" + str(sn) + "/" + file)
+        os.rmdir("data_dump/" + str(yr) + "_" + str(rc) + "_" + str(sn))
+
+    except Exception as exc:
+        import traceback
+        print(traceback.format_exc())
+        print("Error:", str(exc))
+    
 def save_telemetry(yr, rc, sn):
 
     folder_name = f"{yr}_{rc}_{sn}"
@@ -272,7 +343,7 @@ def save_telemetry(yr, rc, sn):
                 try:
                     driver_laps = session.laps.pick_driver(driver)
                     fast = driver_laps[driver_laps['LapNumber'] == int(lap)].iloc[0]
-                    telemetry = fast.get_car_data().add_distance()
+                    telemetry = fast.get_telemetry().add_distance()
                     
                     csv_file_name = f"{yr}_{rc}_{sn}_{driver}_{lap}_telemetry.csv"
                     telemetry.to_csv("data_dump/" + str(yr) + "_" + str(rc) + "_" + str(sn) + "/" + csv_file_name, index=False)
@@ -348,6 +419,25 @@ def get_results(yr, rc, sn):
         
         return None
     
+def get_car_data(yr, rc, sn, driver, lap):
+    
+    try:
+        
+        file_name = f"{yr}_{rc}_{sn}_{driver}_{lap}_car_data.csv"
+        folder_name = f"{yr}_{rc}_{sn}"
+        
+        car_data = read_file("car_data/" + folder_name + "/" + file_name)
+        
+        for col in car_data.columns:
+            if col.lower().__contains__("time"):
+                car_data[col] = pd.to_timedelta(car_data[col])
+
+        return car_data
+    
+    except:
+        
+        return None
+
 def get_telemetry(yr, rc, sn, driver, lap):
     
     try:
@@ -411,6 +501,7 @@ try:
         try:
             # save_from(2018, "laps")
             # save_from(2018, "results")
+            # save_from(2018, "car_data")
             # save_from(2018, "telemetry")
             done = True
         except:
